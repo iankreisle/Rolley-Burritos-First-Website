b5f29f9d1c43c3c80b163410c16312879eedfb3f
Ami04033 <amynguyen819@u.boisestate.edu>
2022-03-29 00:35:30 -0600

    Edited random.js

--- random.js
+++ random.js
@@ -8,14 +8,17 @@ function submitChange(){
     var userInput = document.getElementById("myText").value;
     document.getElementById("submission").innerHTML = userInput
 }
+
 function randomChange(min, max){
     return Math.floor(Math.random() * (max-min)) + min;
 }
+
 function randomCreate(){
-var randomNumber = randomChange(0,35);
+var randomNumber = randomChange(1,9);
 console.log(randomNumber);
 for(let i=0; i<randomNumber; i++){
     const element = document.createElement('div');
     element.innerHTML = '<img src="https://media3.giphy.com/media/lojcDkpTHNU6T1UOCd/giphy_s.gif" alt="budgie">'
-   document.getElementById("pics").appendChild(element);
+    document.getElementById("pics").appendChild(element);
+    
 }}
