ba4b1fa8236ce27e4e6139c3267fa32c9eac88a4
Aspenar <89657089+Aspenar@users.noreply.github.com>
2022-03-28 22:58:33 -0600

    allowed users to add text to the page

--- random.js
+++ random.js
@@ -1,3 +1,10 @@
 function theChange(){
-    document.getElementById("htmlChange").innerHTML = "Haha this is a change!"
-};
+    document.getElementById("htmlChange").innerHTML = "Haha this is a change!";
+}
+function cssChange(){
+    document.getElementById("cssButton").style.backgroundColor = "steelblue";
+}
+function submitChange(){
+    var userInput = document.getElementById("myText").value;
+    document.getElementById("submission").innerHTML = userInput
+}
