ede99ba947c404c0a99493d2b98d1fde81ac7d01
Aspenar <89657089+Aspenar@users.noreply.github.com>
2022-03-28 23:31:20 -0600

    added random buttons

--- random.js
+++ random.js
@@ -8,3 +8,10 @@ function submitChange(){
     var userInput = document.getElementById("myText").value;
     document.getElementById("submission").innerHTML = userInput
 }
+function randomChange(min, max){
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+function randomCreate(){
+var randomNumber = randomChange(0,35);
+console.log(randomNumber);
+}
