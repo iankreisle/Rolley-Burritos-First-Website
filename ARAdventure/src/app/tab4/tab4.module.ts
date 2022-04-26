6db46945d51d73deac6a40d9ea9f99d1ffd9a1e1
Aspenar <89657089+Aspenar@users.noreply.github.com>
2022-04-26 11:57:03 -0600

    fixed error for explore container

--- ARAdventure/src/app/tab4/tab4.module.ts
+++ ARAdventure/src/app/tab4/tab4.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab4Page } from './tab4.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab4PageRoutingModule } from './tab4-routing.module';
 
 @NgModule({
@@ -13,7 +11,6 @@ import { Tab4PageRoutingModule } from './tab4-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab4Page }]),
     Tab4PageRoutingModule,
   ],
