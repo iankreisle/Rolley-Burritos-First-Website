078be4d98877a91d0ad93f6138dd8cc753c5dfee
Ami04033 <amynguyen819@u.boisestate.edu>
2022-04-26 16:36:02 -0600

    Added vibration code and added details to story

--- ARAdventure/src/app/app.module.ts
+++ ARAdventure/src/app/app.module.ts
@@ -6,12 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Vibration } from '@awesome-cordova-plugins/vibration';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
+
+
+
 export class AppModule {}
